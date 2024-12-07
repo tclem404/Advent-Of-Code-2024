6448dd8ec675124af5cff968dcb4d22c2f515749
tclem404 <tclem404@gmail.com>
2024-12-07 13:19:01 -0600

    Day 7

---
+++ day7/day7-2.py
@@ -0,0 +1,26 @@
+import fileinput
+from collections import Counter
+from functools import cmp_to_key
+import re
+
+lines = [list(map(int, s.replace('\n','').replace(':','').split(' '))) for s in list(fileinput.input('day7\input.txt'))]
+
+currCount = 0
+
+def checkVal(line, ind, currSum):
+    if currSum > line[0]:
+        return False
+    if ind == len(line):
+        return currSum == line[0]
+
+    return (checkVal(line, ind + 1, currSum + line[ind]) or 
+            checkVal(line, ind + 1, currSum * line[ind]) or
+            checkVal(line, ind + 1, int(str(currSum) + str(line[ind]))))
+
+
+for line in lines:
+    if checkVal(line, 2, line[1]):
+        currCount += line[0]
+
+
+print(currCount)
