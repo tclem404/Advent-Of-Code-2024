4db9f0f2c6d93cf649badcbf4efc5a975cd361f1
tclem404 <tclem404@gmail.com>
2024-12-17 09:17:12 -0800

    MMixed Up

--- day15/day15-2.py
+++ day15/day15-2.py
@@ -4,10 +4,7 @@ import functools
 import re
 
 lines = [s.replace('\n','') for s in list(fileinput.input('day15\input.txt'))]
-grid = lines[:lines.index("")]
-
-grid = list(map(list, [l.replace('#', "##").replace('O', "[]").replace('.', "..").replace('@', "@.") for l in grid]))
-
+grid = list(map(list, lines[:lines.index("")]))
 
 instructions = lines[lines.index("") + 1:]
 
@@ -15,62 +12,43 @@ startPos = [-1,-1]
 for i, line in enumerate(grid):
     if '@' in line:
         startPos = [i, line.index('@')]
+        grid[i][line.index('@')] = '.'
         break
 
+print(startPos)
 
-def check(x, y, xDir, yDir, partner):
-    if grid[x][y] == '#':
-        return False
-    
-    if grid[x][y] == '.':
-        return True
-
-    result1 = check(x + xDir, y + yDir, xDir, yDir, True)
-
-    if yDir == 0 and partner:
-        if result1 and grid[x][y] == ']':
-            return check(x, y - 1, xDir, yDir, False)
-        if result1 and grid[x][y] == '[':
-            return check(x, y + 1, xDir, yDir, False)
-    
-    return result1
 
 def mov(x, y, xDir, yDir):
     if grid[x][y] == '#':
-        return
+        return -1
     
     if grid[x][y] == '.':
-        return
-    
-    mov(x + xDir, y + yDir, xDir, yDir)
-    grid[x + xDir][y + yDir] = grid[x][y]
-    prev = grid[x][y]
-    grid[x][y] = '.'
+        return 1
 
-    if xDir != 0:
-        if prev == '[':
-            mov(x, y + 1, xDir, yDir)
-        if prev == ']':
-            mov(x, y - 1, xDir, yDir)
+    result = mov(x + xDir, y + yDir, xDir, yDir)
+    
+    if result != -1:
+        grid[x + xDir][y + yDir] = grid[x][y]
+    
+    return result
 
 for line in instructions:
     for movement in line:
-
         xDir = (movement == 'v') - (movement == '^')
         yDir = (movement == '>') - (movement == '<')
 
-        result = check(startPos[0] + xDir, startPos[1] + yDir, xDir, yDir, True)
+        result = mov(startPos[0] + xDir, startPos[1] + yDir, xDir, yDir)
 
-        if result:
-            mov(startPos[0], startPos[1], xDir, yDir)
-            startPos = [startPos[0] + xDir, startPos[1] + yDir]
+        if result != -1:
+            grid[startPos[0]][startPos[1]] = '.'
+            
+            startPos[0] += xDir
+            startPos[1] += yDir
 
-
-for line2 in grid:
-    print(''.join(line2))
+            grid[startPos[0]][startPos[1]] = '@'
 
 currSum = 0
 for i, line in enumerate(grid):
-    currSum += sum([i * 100 + j for j in range(len(line)) if line[j] == '['])
+    currSum += sum([i * 100 + j for j in range(len(line)) if line[j] == 'O'])
 
 print(currSum)
